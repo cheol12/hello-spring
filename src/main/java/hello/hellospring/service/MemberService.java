613c09224e5c463ea3ff0b774388e2cf6170cd9c
ddongman <sky66951@naver.com>
2023-06-12 20:39:15 +0900

    스프링 입문 - 코드로 배우는 스프링 부트, 웹 MVC, DB 접근 기술 12강 - 회원 서비스 테스트 끝

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -9,7 +9,12 @@ import java.util.Optional;
 
 public class MemberService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+    // 레포지토리 객체를 직접 생성하는 것이 아니라 외부에서 가져오는 방식.
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     /**
      * 회원 가입
--- src/test/java/hello/hellospring/service/MemberServiceTest.java
+++ src/test/java/hello/hellospring/service/MemberServiceTest.java
@@ -1,7 +1,10 @@
 package hello.hellospring.service;
 
 import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemoryMemberRepository;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -9,20 +12,64 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class MemberServiceTest {
 
-    MemberService memberService = new MemberService();
+    MemberService memberService;
+    MemoryMemberRepository memberRepository;
+
+    @BeforeEach
+    public void beforeEach(){
+        memberRepository = new MemoryMemberRepository();
+        memberService = new MemberService(memberRepository);
+    }
+
+    @AfterEach
+    public void afterEach(){
+        memberRepository.clearStore();
+    }
+    // 테스트 실행시 순서 의존성을 없애기 위해 실행한 데이터들을 깔끔히 지워주는 함수를 만들고 가져온다.
+
 
     @Test
     void 회원가입() {
         // given 뭔가가 주어졌는데
         Member member = new Member();
-        member.setName("hello");
+        member.setName("spring");    // hello가 주어졌고
 
         // when 이걸 실행했을 때
-        Long saveId = memberService.join(member);
+        Long saveId = memberService.join(member);   // 멤버 정보를 꺼냈을 때
 
         // then 결과가 이것으로 나와야 한다
         Member findMember = memberService.findOne(saveId).get();
         assertThat(member.getName()).isEqualTo(findMember.getName());
+    }
+
+    @Test
+    public void 중복_회원_예외(){
+        //given
+        Member member1 = new Member();
+        member1.setName("spring");
+
+        Member member2 = new Member();
+        member2.setName("spring");
+
+        //when
+        memberService.join(member1);    // join 절차 = 중복검사 -> 가입
+        IllegalStateException e = assertThrows(IllegalStateException.class, () ->
+                memberService.join(member2));
+        assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+
+
+
+
+//        try{
+//            memberService.join(member2);
+//            fail();
+//        }catch (IllegalStateException e){
+//            assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.123123");
+//        }
+
+        //then
+
+
     }
 
     @Test
